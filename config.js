6a7547122120b5ed63ac3578b2cde45300e493dd
chasingSublimity <blakemsager@gmail.com>
2017-02-16 15:17:58 -0600

    debug heroku error

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://chasingSublimity1:fancyPassword@ds151927.mlab.com:51927/setlist-generator';
+                      'mongodb://chasingSublimity:lavalamp1@ds151927.mlab.com:51927/setlist-generator';
 exports.TEST_DATABASE_URL = (
 											process.env.TEST_DATABASE_URL ||
 											'mongodb://localhost/test-setlist-generator');
